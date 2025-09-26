4037d8c8bb1e92599842c313cfb63e92fd1c3a11
Bartek Kus <7887446+bartekus@users.noreply.github.com>
2025-09-26 16:03:20 -0600

    Fix frontend CI issue

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,19 +3,17 @@ import react from '@vitejs/plugin-react'
 import viteTsConfigPaths from "vite-tsconfig-paths"
 import tailwindcss from "@tailwindcss/vite"
 
-if (process.env && !process.env.VITE_WEB_DOMAIN) {
+if (process.env && process.env.NODE_ENV === "development" && !process.env.VITE_WEB_DOMAIN) {
     throw new Error("You must specify a valid VITE_WEB_DOMAIN.");
 }
-
-const WEB_DOMAIN = process.env.VITE_WEB_DOMAIN;
 
 // https://vite.dev/config/
 const config = defineConfig({
     base: '/',                             // change if host under a subpath
     server: {
         host: true,
-        hmr: { host: `${WEB_DOMAIN}` },     // helpful when behind Traefik
-        origin: `https://${WEB_DOMAIN}`,    // fixes HMR ws origin in some setups
+        hmr: { host: `${process.env.VITE_WEB_DOMAIN}` },     // helpful when behind Traefik
+        origin: `https://${process.env.VITE_WEB_DOMAIN}`,    // fixes HMR ws origin in some setups
     },
     plugins: [
         // this is the plugin that enables path aliases
