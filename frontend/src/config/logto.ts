8125baa03c3afbc4d97a54f8051171830d32d646
Bartek Kus <7887446+bartekus@users.noreply.github.com>
2025-09-27 22:35:55 -0600

    Fix frontend env vars

--- frontend/src/config/logto.ts
+++ frontend/src/config/logto.ts
@@ -3,26 +3,29 @@ import { LogtoConfig } from '@logto/react'
 console.log('import.meta.env');
 console.log(import.meta.env)
 
-if (import.meta.env && !import.meta.env.VITE_API_DOMAIN) {
+console.log('window?.__ENV__');
+console.log(window?.__ENV__)
+
+const API_DOMAIN = import.meta.env?.VITE_API_DOMAIN || window?.__ENV__?.VITE_API_DOMAIN;
+const LOGTO_APP_ID = import.meta.env?.VITE_LOGTO_APP_ID || window?.__ENV__?.VITE_LOGTO_APP_ID;
+const LOGTO_DOMAIN = import.meta.env?.VITE_LOGTO_DOMAIN || window?.__ENV__?.VITE_LOGTO_DOMAIN;
+const WEB_DOMAIN = import.meta.env?.VITE_WEB_DOMAIN || window?.__ENV__?.VITE_WEB_DOMAIN;
+
+if (!API_DOMAIN) {
     throw new Error("You must specify a valid VITE_API_DOMAIN.");
 }
 
-if (import.meta.env && !import.meta.env.VITE_LOGTO_APP_ID) {
+if (!LOGTO_APP_ID) {
     throw new Error("You must specify a valid VITE_LOGTO_APP_ID.");
 }
 
-if (import.meta.env && !import.meta.env.VITE_LOGTO_DOMAIN) {
+if (!LOGTO_DOMAIN) {
     throw new Error("You must specify a valid VITE_LOGTO_DOMAIN.");
 }
 
-if (import.meta.env && !import.meta.env.VITE_WEB_DOMAIN) {
+if (!WEB_DOMAIN) {
     throw new Error("You must specify a valid VITE_WEB_DOMAIN.");
 }
-
-const API_DOMAIN = import.meta.env.VITE_API_DOMAIN || window.__ENV__.VITE_API_DOMAIN;
-const LOGTO_APP_ID = import.meta.env.VITE_LOGTO_APP_ID || window.__ENV__.VITE_LOGTO_APP_ID;
-const LOGTO_DOMAIN = import.meta.env.VITE_LOGTO_DOMAIN || window.__ENV__.VITE_LOGTO_DOMAIN;
-const WEB_DOMAIN = import.meta.env.VITE_WEB_DOMAIN || window.__ENV__.VITE_WEB_DOMAIN;
 
 export const config: LogtoConfig = {
     // The public Logto endpoint (core)
